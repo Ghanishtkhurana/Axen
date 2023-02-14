5eea595208bf608be87f7d425d1eea97c246c9e5
Ghanishtkhurana <101393463+Ghanishtkhurana@users.noreply.github.com>
2023-02-14 21:50:27 +0530

    add product form done

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const productSchema = new mongoose.Schema({
     title : {type : String ,required : true},
-    price : {type : String ,required : true},
+    price : {type : String },
     price2 : {type : Number ,required : true},
     strik : {type : String ,required : true},
     img : {type : Array ,required : true},
