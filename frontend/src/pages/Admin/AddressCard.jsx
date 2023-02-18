2c0fd14e5dcad0869ef38100e6e05fa01cfa0283
Ghanishtkhurana <101393463+Ghanishtkhurana@users.noreply.github.com>
2023-02-19 00:39:57 +0530

    showing product details in user address details card functionality done

--- frontend/src/pages/Admin/AddressCard.jsx
+++ frontend/src/pages/Admin/AddressCard.jsx
@@ -10,6 +10,8 @@ import {
   AlertDialogCloseButton,
   Text,
   Box,
+  Flex,
+  Image,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -37,11 +39,13 @@ const AddressCard = ({ id, isOpen, onOpen, onClose, cancelRef }) => {
     getAddress(id).then((res) => setData(res));
   }, [id]);
 
-  console.log(data);
+  //   console.log(data);
+  //   console.log("cart", data.cart);
   return (
     <>
       <AlertDialog
-        motionPreset="slideInBottom"
+        // motionPreset="slideInBottom"
+        motionPreset="scale"
         leastDestructiveRef={cancelRef}
         onClose={onClose}
         isOpen={isOpen}
@@ -50,28 +54,88 @@ const AddressCard = ({ id, isOpen, onOpen, onClose, cancelRef }) => {
         <AlertDialogOverlay />
 
         <AlertDialogContent>
-          <AlertDialogHeader>Checkout</AlertDialogHeader>
+          <AlertDialogHeader fontSize={"18px"}>Order Details</AlertDialogHeader>
           <AlertDialogCloseButton />
           <AlertDialogBody>
-            {data && (
+            {/* <Flex> */}
+            <Text fontSize={"14px"} fontWeight={"500"}>
+              Address
+            </Text>
+            {data !== undefined && (
               <Box>
-                <Text>Name : {data.name}</Text>
-                <Text>Mobile No : {data.mobileNumber}</Text>
-                <Text>Pin Code : {data.pinCode}</Text>
-                <Text>House No : {data.houseNo}</Text>
-                <Text>Area : {data.area}</Text>
-                <Text>Landmark : {data.landmark}</Text>
-                <Text>State : {data.state}</Text>
-                <Text>User-Id : {data.userId}</Text>
+                {data && (
+                  <Flex p={2} gap={10} border={"1px solid"} borderColor={"gray.200"}>
+                    <Box>
+                      <Text fontWeight={500} fontSize={"13px"}>
+                        Name : {data.name}
+                      </Text>
+                      <Text fontWeight={500} fontSize={"13px"}>
+                        Mobile No : {data.mobileNumber}
+                      </Text>
+                      <Text fontWeight={500} fontSize={"13px"}>
+                        Pin Code : {data.pinCode}
+                      </Text>
+                      <Text fontWeight={500} fontSize={"13px"}>
+                        House No : {data.houseNo}
+                      </Text>
+                    </Box>
+                    <Box>
+                      <Text fontWeight={500} fontSize={"13px"}>
+                        Area : {data.area}
+                      </Text>
+                      <Text fontWeight={500} fontSize={"13px"}>
+                        Landmark : {data.landmark}
+                      </Text>
+                      <Text fontWeight={500} fontSize={"13px"}>
+                        State : {data.state}
+                      </Text>
+                      <Text fontWeight={500} fontSize={"13px"}>
+                        User-Id : {data.userId}
+                      </Text>
+                    </Box>
+                  </Flex>
+                )}
+                <Box mt={1} border={"1px solid"} borderColor={"gray.200"}>
+                  <Text
+                    fontWeight={500}
+                    noOfLines={1}
+                    fontSize={"13px"}
+                  >
+                    Products
+                  </Text>
+                  {data.cart &&
+                    data.cart.map((post, i) => (
+                      <Flex  borderBottomColor={"gray.100"} p={1}>
+                        <Box w={"15%"}>
+                          <Image w={"40px"} src={post.img[0]} />
+                        </Box>
+                        <Box w={"85%"}>
+                          <Text
+                            fontWeight={500}
+                            noOfLines={1}
+                            color={"gray.600"}
+                            fontSize={"13px"}
+                          >
+                            {post.title}
+                          </Text>
+                          <Text
+                            fontWeight={500}
+                            color={"gray.600"}
+                            fontSize={"13px"}
+                          >
+                            {"₹"}{" "}{post.price2.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                          </Text>
+                        </Box>
+                      </Flex>
+                    ))}
+                </Box>
               </Box>
             )}
+            {/* </Flex> */}
           </AlertDialogBody>
           <AlertDialogFooter>
             <Button ref={cancelRef} onClick={onClose}>
-              No
-            </Button>
-            <Button bgColor={"purple.400"} color={"white"} ml={3}>
-              Proceed to Checkout
+              Close
             </Button>
           </AlertDialogFooter>
         </AlertDialogContent>
